fcd1a5bbdd190fa2f1a27ac09a13d5b00159d2d4
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-03 14:48:05 +0000

    added create note btn styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,11 @@
 import styles from './Button.module.css';
 
-function Button({}) {
-  return <button>BUTTON</button>;
+interface ButtonProps {
+  text: string;
+}
+
+function Button({ text }: ButtonProps) {
+  return <button className={styles.button}>{text}</button>;
 }
 
 export default Button;
