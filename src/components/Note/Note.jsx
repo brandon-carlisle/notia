b79f965d8f9b98e6c11872195c26c0c2f7a2f47a
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-16 17:51:37 +0000

    styling for heading of notes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -3,15 +3,15 @@ import ReactMarkdown from 'react-markdown';
 import styles from './Note.module.css';
 
 function Note({ note }) {
-  const date = new Intl.DateTimeFormat('en-gb', { dateStyle: 'medium' }).format(
+  const date = new Intl.DateTimeFormat('en-gb', { dateStyle: 'long' }).format(
     new Date(note.dateCreated),
   );
 
   return (
     <div>
       <div className={styles.heading}>
-        <h1>{note.title}</h1>
-        <p>{date}</p>
+        <p className={styles.title}>{note.title}</p>
+        <p className={styles.date}>{date}</p>
       </div>
 
       <ReactMarkdown>{note.content}</ReactMarkdown>
