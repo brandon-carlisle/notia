ae9afc0c8b4476abed1c084aed082bf8c94b8f95
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-16 17:44:11 +0000

    fixed date formatting on note component

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,13 +1,20 @@
 import ReactMarkdown from 'react-markdown';
 
+import styles from './Note.module.css';
+
 function Note({ note }) {
+  const date = new Intl.DateTimeFormat('en-gb', { dateStyle: 'medium' }).format(
+    new Date(note.dateCreated),
+  );
+
   return (
     <div>
-      <h1>{note.title}</h1>
+      <div className={styles.heading}>
+        <h1>{note.title}</h1>
+        <p>{date}</p>
+      </div>
 
       <ReactMarkdown>{note.content}</ReactMarkdown>
-
-      <p>Date: {note.dateCreated}</p>
     </div>
   );
 }
