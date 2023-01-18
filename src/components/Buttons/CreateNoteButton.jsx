f2fffe1843599fc68e9a11fa704d0f376d0f3bbd
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-18 17:02:43 +0000

    added click animation for create note btn + styled

--- src/components/Buttons/CreateNoteButton.jsx
+++ src/components/Buttons/CreateNoteButton.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 function CreateNoteButton() {
   return (
     <Link
-      className="inline-block fixed right-8 bottom-8 rounded-full bg-gray-900 w-12 h-12"
+      className="inline-block fixed right-8 bottom-8 rounded-full bg-gray-900 w-12 h-12 active:translate-y-1 transition-all"
       to="/"
     >
       <div className="relative w-full h-full">
-        <div className="absolute top-1/2 left-1/2 rounded bg-gray-100 w-1/2 h-1 -translate-x-1/2 -translate-y-1/2"></div>
-        <div className="absolute top-1/2 left-1/2 rounded bg-gray-100 w-1/2 h-1 -translate-x-1/2 -translate-y-1/2 rotate-90"></div>
+        <div className="absolute top-1/2 left-1/2 rounded bg-white w-1/2 h-[3px] -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute top-1/2 left-1/2 rounded bg-white w-1/2 h-[3px] -translate-x-1/2 -translate-y-1/2 rotate-90"></div>
       </div>
     </Link>
   );
