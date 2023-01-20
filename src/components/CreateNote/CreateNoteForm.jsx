1bb41ce7c705cf73f3a959bb2ee454d68dd5a11d
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-20 17:09:03 +0000

    Createnote page styling

--- src/components/CreateNote/CreateNoteForm.jsx
+++ src/components/CreateNote/CreateNoteForm.jsx
@@ -2,14 +2,27 @@ import { Form } from 'react-router-dom';
 
 function CreateNoteForm() {
   return (
-    <Form method="post">
-      <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" />
+    <Form method="post" className="flex flex-col gap-3">
+      <input
+        type="text"
+        id="title"
+        name="title"
+        className="px-4 py-2 border border-gray-200"
+      />
 
-      <label htmlFor="content">content</label>
-      <textarea type="text" id="content" name="content" />
+      <textarea
+        type="text"
+        id="content"
+        name="content"
+        className="p-4 border border-gray-200"
+        rows="20"
+      />
 
-      <button text="Create note" type="submit">
+      <button
+        text="Create note"
+        type="submit"
+        className="bg-gray-900 text-white w-32 px-4 py-2 rounded-md"
+      >
         Submit
       </button>
     </Form>
