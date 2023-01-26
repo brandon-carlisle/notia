85664923a3a71a8ec88f956bb330cd6fe6259430
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-26 17:34:43 +0000

    Added basic home button to sidebare

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { TbHome } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
 import SidebarItem from './SidebarItem';
@@ -12,7 +13,11 @@ function Sidebar({ notes }) {
   }
 
   return (
-    <div className="flex flex-col gap-8 py-12 px-4 w-1/5 overflow-y-auto h-screen border-r-2">
+    <div className="flex flex-col justify-between items-center gap-8 py-12 px-4 w-1/5 overflow-y-auto h-screen border-r-2">
+      <Link to="/">
+        <TbHome className="text-3xl" />
+      </Link>
+
       {notes && notes.length > 0 && (
         <ul className="flex flex-col gap-4 w-full list-none">
           {notesSortedByDate.map((note) => (
