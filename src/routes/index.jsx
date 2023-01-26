83fbcf4f86b431cd11cfefe9a13f6ae78277435c
beanziii <93879641+beanziii@users.noreply.github.com>
2023-01-26 17:47:33 +0000

    added info to index page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,8 +9,27 @@ function Index() {
           <h2 className="font-semibold">create markdown notes with ease.</h2>
         </div>
       </header>
-
       <Features />
+
+      <ul className="list-disc mt-16 list-inside">
+        <li>
+          Notia uses the CommonMark spec to format any Markdown that you write
+        </li>
+        <li>
+          If you are unsure on how to write Markdown, please use{' '}
+          <a
+            href="https://commonmark.org/help/"
+            className="underline"
+            target="_blank"
+          >
+            this short guide
+          </a>{' '}
+          to get started
+        </li>
+        <li>
+          Use the button in the bottom right of your screen to create a note
+        </li>
+      </ul>
     </main>
   );
 }
