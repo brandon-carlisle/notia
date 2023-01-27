c09998ebaacd948700b2765096b5faa2fe585e2f
Brandon <93879641+beanziii@users.noreply.github.com>
2023-01-27 14:28:37 +0000

    Can now see active note link in sidebar

--- src/components/Sidebar/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem.jsx
@@ -1,15 +1,21 @@
 import { toast } from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function SidebarItem({ title, id }) {
   return (
-    <li
-      className="rounded-md text-center break-words hover:bg-gray-300 w-full"
-      onClick={() => toast.dismiss()}
-    >
-      <Link to={`/${id}`}>
-        <p className="w-full h-full py-2 capitalize">{title}</p>
-      </Link>
+    <li className="" onClick={() => toast.dismiss()}>
+      <NavLink
+        to={`/${id}`}
+        className={({ isActive, isPending }) =>
+          isActive
+            ? 'bg-gray-200 sidebar-item'
+            : isPending
+            ? 'pending'
+            : 'sidebar-item'
+        }
+      >
+        {title}
+      </NavLink>
     </li>
   );
 }
